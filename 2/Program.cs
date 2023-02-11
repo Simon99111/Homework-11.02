0cf6db53371bced5a9ac5dcc93481c2d948ee82a
Семён Курочкин <sema.kurochkin@mail.ru>
2023-02-11 20:41:08 +0300

    Программа 2

---
+++ 2/Program.cs
@@ -0,0 +1,24 @@
+﻿int a = 8;
+int b = 8;
+int max = a;
+int min = b;
+
+if (max == min) Console.WriteLine("Числа равны");
+
+if (max > min)
+{
+    Console.Write("max = ");
+    Console.Write(max);
+    Console.WriteLine();
+    Console.Write("min = ");
+    Console.Write(min);
+}
+
+if (max < min)
+{
+    Console.Write("max=");
+    Console.Write(min);
+    Console.WriteLine();
+    Console.Write("min=");
+    Console.Write(max);
+}
